c2d7da58bc46a2295a10223fccbbb88b2bde14f1
qhtjd8989 <qhtjd8899@naver.com>
2022-07-13 19:23:19 +0900

    springboot 첫수업

---
+++ src/test/java/com/springboot/studybosung/StudyBosungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studybosung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBosungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
